d743d4610054c7a7a01ee7c3a243e3d3492aedb1
Ryosuke Okuta <okuta@preferred.jp>
2016-04-19 22:20:13 +0900

    Add tests of float16 and float64 in functions.array

--- tests/chainer_tests/functions_tests/array_tests/test_select_item.py
+++ tests/chainer_tests/functions_tests/array_tests/test_select_item.py
@@ -11,19 +11,21 @@ from chainer.testing import attr
 
 
 @testing.parameterize(
-    {'in_shape': (10, 5), 'out_shape': (10,)},
-    {'in_shape': (0, 5),  'out_shape': (0,)},
-    {'in_shape': (1, 33), 'out_shape': (1,)},
+    {'in_shape': (10, 5), 'out_shape': (10,), 'dtype': numpy.float32},
+    {'in_shape': (0, 5),  'out_shape': (0,), 'dtype': numpy.float32},
+    {'in_shape': (1, 33), 'out_shape': (1,), 'dtype': numpy.float32},
+    {'in_shape': (10, 5), 'out_shape': (10,), 'dtype': numpy.float16},
+    {'in_shape': (10, 5), 'out_shape': (10,), 'dtype': numpy.float64},
 )
 class TestSelectItem(unittest.TestCase):
 
     def setUp(self):
         self.x_data = numpy.random.uniform(
-            -1, 1, self.in_shape).astype(numpy.float32)
+            -1, 1, self.in_shape).astype(self.dtype)
         self.t_data = numpy.random.randint(
             0, 2, self.out_shape).astype(numpy.int32)
         self.gy_data = numpy.random.uniform(
-            -1, 1, self.out_shape).astype(numpy.float32)
+            -1, 1, self.out_shape).astype(self.dtype)
 
     def check_forward(self, x_data, t_data):
         x = chainer.Variable(x_data)
@@ -31,6 +33,7 @@ class TestSelectItem(unittest.TestCase):
         y = functions.select_item(x, t)
         y_exp = cuda.to_cpu(x_data)[range(t_data.size), cuda.to_cpu(t_data)]
 
+        self.assertEqual(y.data.dtype, self.dtype)
         numpy.testing.assert_equal(cuda.to_cpu(y.data), y_exp)
 
     def test_forward_cpu(self):
@@ -41,19 +44,27 @@ class TestSelectItem(unittest.TestCase):
         self.check_forward(cuda.to_gpu(self.x_data),
                            cuda.to_gpu(self.t_data))
 
-    def check_backward(self, x_data, t_data, gy_data):
-        gradient_check.check_backward(
-            functions.SelectItem(),
-            (x_data, t_data), gy_data, eps=0.01)
+    def check_backward(self, x_data, t_data, gpu):
+        x = chainer.Variable(x_data)
+        t = chainer.Variable(t_data)
+        y = functions.select_item(x, t)
+        y.grad = y.data
+        y.backward()
+        gx = numpy.zeros_like(cuda.to_cpu(x_data))
+        ind = numpy.arange(t_data.size), cuda.to_cpu(t_data)
+        gx[ind] = cuda.to_cpu(x_data)[ind]
+        if gpu:
+            gx = cuda.to_gpu(gx)
+        gradient_check.assert_allclose(gx, x.grad, atol=0, rtol=0)
 
     def test_backward_cpu(self):
-        self.check_backward(self.x_data, self.t_data, self.gy_data)
+        self.check_backward(self.x_data, self.t_data, gpu=False)
 
     @attr.gpu
     def test_backward_gpu(self):
         self.check_backward(cuda.to_gpu(self.x_data),
                             cuda.to_gpu(self.t_data),
-                            cuda.to_gpu(self.gy_data))
+                            gpu=True)
 
 
 @testing.parameterize(
--- tests/chainer_tests/functions_tests/array_tests/test_where.py
+++ tests/chainer_tests/functions_tests/array_tests/test_where.py
@@ -4,40 +4,39 @@ import numpy
 
 import chainer
 from chainer import cuda
-import chainer.functions as F
+from chainer import functions
 from chainer import gradient_check
 from chainer import testing
 from chainer.testing import attr
 
 
+@testing.parameterize(*testing.product({
+    'shape': [(3, 2, 4)],
+    'dtype': [numpy.float16, numpy.float32, numpy.float32],
+}))
 class TestWhere(unittest.TestCase):
-
-    shape = (3, 2, 4)
 
     def setUp(self):
         self.c_data = numpy.random.uniform(-1, 1, self.shape) > 0
         self.x_data = \
-            numpy.random.uniform(-1, 1, self.shape).astype(numpy.float32)
+            numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
         self.y_data = \
-            numpy.random.uniform(-1, 1, self.shape).astype(numpy.float32)
-        self.g_data = \
-            numpy.random.uniform(-1, 1, self.shape).astype(numpy.float32)
+            numpy.random.uniform(-1, 1, self.shape).astype(self.dtype)
 
     def check_forward(self, c_data, x_data, y_data):
         c = chainer.Variable(c_data)
         x = chainer.Variable(x_data)
         y = chainer.Variable(y_data)
 
-        z = F.where(c, x, y)
+        z = functions.where(c, x, y)
 
         self.assertEqual(x.data.shape, z.data.shape)
 
-        for c, x, y, z in zip(c.data.flatten(), x.data.flatten(),
-                              y.data.flatten(), z.data.flatten()):
-            if c:
-                self.assertEqual(x, z)
+        for i in numpy.ndindex(c.data.shape):
+            if c.data[i]:
+                self.assertEqual(x.data[i], z.data[i])
             else:
-                self.assertEqual(y, z)
+                self.assertEqual(y.data[i], z.data[i])
 
     def test_forward_cpu(self):
         self.check_forward(self.c_data, self.x_data, self.y_data)
@@ -48,19 +47,27 @@ class TestWhere(unittest.TestCase):
                            cuda.to_gpu(self.x_data),
                            cuda.to_gpu(self.y_data))
 
-    def check_backward(self, c_data, x_data, y_data, g_data):
-        gradient_check.check_backward(
-            F.Where(), (c_data, x_data, y_data), g_data)
+    def check_backward(self, c_data, x_data, y_data):
+        c = chainer.Variable(c_data)
+        x = chainer.Variable(x_data)
+        y = chainer.Variable(y_data)
+        z = functions.where(c, x, y)
+        z.grad = z.data
+        z.backward()
+        xp = cuda.get_array_module(c_data)
+        gx = xp.where(c_data, x_data, 0)
+        gy = xp.where(c_data, 0, y_data)
+        gradient_check.assert_allclose(gx, x.grad, atol=0, rtol=0)
+        gradient_check.assert_allclose(gy, y.grad, atol=0, rtol=0)
 
     def test_backward_cpu(self):
-        self.check_backward(self.c_data, self.x_data, self.y_data, self.g_data)
+        self.check_backward(self.c_data, self.x_data, self.y_data)
 
     @attr.gpu
     def test_backward_gpu(self):
         self.check_backward(cuda.to_gpu(self.c_data),
                             cuda.to_gpu(self.x_data),
-                            cuda.to_gpu(self.y_data),
-                            cuda.to_gpu(self.g_data))
+                            cuda.to_gpu(self.y_data))
 
 
 testing.run_module(__name__, __file__)
